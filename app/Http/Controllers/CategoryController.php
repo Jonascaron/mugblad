6b8f30d4401739309c715949855995f5d55855fe
Jonas Caron <jonas.caron16@gmail.com>
2024-01-25 20:32:05 +0100

    added auth to the controllers

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,7 +6,12 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
-{
+{   
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index() {
         $data = [
             'categories' => Category::all()
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Posts;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
         $data = [
             'posts' => Posts::all(),
@@ -17,17 +23,30 @@ class PostController extends Controller
     public function create() {
         $data = [
             'posts' => Posts::all(),
+            'categories' => Category::all(),
         ];
         return view('posts.create', $data);
     }
 
     public function store(Request $request) {
+
+        //dd($request->all());
+
+        $request->validate([
+            'img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $imageName = time().'.'.$request->img->extension();
+
+        $request->img->move(public_path('images'), $imageName);
+
         $Posts = new Posts();
         $Posts->title = $request->title;
+        $Posts->image_path = 'images/' . $imageName;
         $Posts->description = $request->description;
-        $Posts->cook_time = $request->cook_time;
+        $Posts->user_id = $request->user;
         $Posts->category_id = $request->category;
         $Posts->save();
-        return redirect('/recipes')->with('status', "Resept is gemaakt");
+        return redirect('/posts')->with('status', "post is gemaakt");
     }
 }
